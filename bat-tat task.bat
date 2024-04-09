c7f28916367a3d84a8d4c0c417a07bb1e7904bbc
ThienCNTT <45612350+thiencntt@users.noreply.github.com>
2024-04-09 10:19:18 +0700

    Add files via upload

---
+++ bat-tat task.bat
@@ -0,0 +1,3 @@
+@echo off
+
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f
---
+++ thietlapiptinh.bat
@@ -0,0 +1,2 @@
+netsh interface ip set address name= "Ethernet" static 192.168.1.16 255.255.255.0 192.168.1.1
+netsh interface ip set dns name= "Ethernet" static 192.168.1.1
